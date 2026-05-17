375b78f9b3359e7f06cb52ff9872e11b7044ca6a
zaftyslogistics-code <zaftyslogistics@gmail.com>
2026-05-17 11:45:38 +0530

    fix: Add AppLogger import to supplier_load_repository_backend.dart

--- TranZfort/lib/src/features/supplier/data/supplier_load_repository_backend.dart
+++ TranZfort/lib/src/features/supplier/data/supplier_load_repository_backend.dart
@@ -1,5 +1,6 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import '../../../core/logger/app_logger.dart';
 import 'supplier_load_models.dart';
 
 abstract class SupplierLoadBackend {
